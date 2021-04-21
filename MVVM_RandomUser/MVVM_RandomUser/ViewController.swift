8777433d8845b89304ece110231a729f5742e94c
Zhenya-Petrovskyi-Axon <epetrovskyi@axon.dev>
2021-04-21 16:41:15 +0300

    Initial project setup

---
+++ MVVM_RandomUser/MVVM_RandomUser/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM_RandomUser
+//
+//  Created by Evhen Petrovskyi on 21.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
