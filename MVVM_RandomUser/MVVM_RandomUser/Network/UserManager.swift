29592c2ef438117af0688246b5be6ca47d4c6a73
Zhenya-Petrovskyi-Axon <epetrovskyi@axon.dev>
2021-04-21 18:26:19 +0300

    User Manager set up

--- MVVM_RandomUser/MVVM_RandomUser/Network/UserManager.swift
+++ MVVM_RandomUser/MVVM_RandomUser/Network/UserManager.swift
@@ -9,19 +9,63 @@ import Foundation
 
 enum NetworkError: Error {
     case urlIsNotValid
+    case noDataToRecieve
+    case wrongResponse
+    case unableToDecode
+    case responseIsNotValid(Int)
 }
 
 protocol UserManagerProtocol: class {
-    func getUsers(completion: (Result<[User], NetworkError>) -> Void)
+    func getUsers(pagination: Bool, completion: @escaping (Result<[User], NetworkError>) -> Void)
 }
 
 class UserManager: UserManagerProtocol {
-
-    private let endpointUrl = ""
-    private let perPage = 20
+    
+    private let baseUrl = "https://randomuser.me/api/?seed=abc"
+    private let resulsPerPage = "&results=20"
+    private let resultsForPage = "&page="
     private let currentPage = 1
     
-    func getUsers(completion: (Result<[User], NetworkError>) -> Void) {
+    private let session = URLSession.shared
+    private let decoder = JSONDecoder()
+    
+    func getUsers(pagination: Bool = false, completion: @escaping (Result<[User], NetworkError>) -> Void) {
+        
+        let fullUrl = "\(baseUrl)\(resulsPerPage)\(resultsForPage)\(currentPage)"
+        
+        guard let url = URL(string: fullUrl) else {
+            completion(.failure(.urlIsNotValid))
+            return
+        }
+        
+        session.dataTask(with: url) { data, reponse, error in
+            
+            if error != nil {
+                completion(.failure(.noDataToRecieve))
+                return
+            }
+            
+            guard let response = reponse as? HTTPURLResponse else {
+                completion(.failure(.wrongResponse))
+                return
+            }
+            
+            switch response.statusCode {
+            case 200...201:
+                guard let data = data else {
+                    completion(.failure(.noDataToRecieve))
+                    return
+                }
+                do {
+                    let users = try self.decoder.decode(Users.self, from: data)
+                    completion(.success(users.results))
+                } catch  {
+                    completion(.failure(.unableToDecode))
+                }
+            default:
+                completion(.failure(.responseIsNotValid(response.statusCode)))
+            }
+        }.resume()
         
     }
 }
