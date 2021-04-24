a9f90a6c5fc6fa3ebeb1c911c89f6c73b8cfdaa9
Zhenya-Petrovskyi-Axon <epetrovskyi@axon.dev>
2021-04-24 22:32:53 +0300

    Details VC file setup

---
+++ MVVM_RandomUser/MVVM_RandomUser/DetailViewModule/UserDetailsVC.swift
@@ -0,0 +1,26 @@
+//
+//  UserDetailsVC.swift
+//  MVVM_RandomUser
+//
+//  Created by Evhen Petrovskyi on 24.04.2021.
+//
+
+import UIKit
+
+class UserDetailsVC: ViewController {
+    
+    @IBOutlet weak var userImage: UIImageView!
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var userGenderLabel: UILabel!
+    @IBOutlet weak var userDobLabel: UILabel!
+    @IBOutlet weak var userNationalityLabel: UILabel!
+    @IBOutlet weak var cellPhoneLabel: UILabel!
+    
+    var user: [User] = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+   
+}
